958fa19cf52f3615efcd3739d0c88f4bc7509fdb
MarinaManushina <122769289+MarinaManushina@users.noreply.github.com>
2023-06-03 08:11:20 +0300

    Update Main.java

--- lb5/src/Main.java
+++ lb5/src/Main.java
@@ -12,7 +12,14 @@ public class Main {
     }
 
     public static void main(String args[]) {
-        int[] sortArr = {12, 6, 4, 1, 15, 10};
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Количество элементов массива: ");
+        int count = scanner.nextInt();
+        int[] sortArr = new int[count];
+        for (int i = 0; i < sortArr.length; i++){
+            System.out.print("Число № " + i + ": ");
+            sortArr[i] = scanner.nextInt();
+        }
         bubbleSort(sortArr);
         for(int i = 0; i < sortArr.length; i++){
             System.out.print(sortArr[i] + "\n");
